bf4203d6dcb9f36c2f57243357c04196bf106aae
IAyrtontwk <ignacioaravena.2020@twk.cl>
2020-12-05 00:58:01 -0300

    Primeros componentes

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h2> Coontenido Home </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h2> Coontenido Menu </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style scoped>
+
+</style>
